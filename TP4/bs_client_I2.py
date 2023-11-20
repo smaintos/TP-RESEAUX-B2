3a7f6536e5c29e5bf051d4f1ff86be075707ab65
MARIAMON <smaintos@MacBook-Air-de-smaintos.local>
2023-11-20 09:49:27 +0100

    aller

--- TP4/bs_client_I2.py
+++ TP4/bs_client_I2.py
@@ -3,20 +3,20 @@ import sys
 
 # On définit la destination de la connexion
 host = '172.16.40.8'  # IP du serveur
-port = 13337              # Port choisir par le serveur
+port = 13337               # Port choisir par le serveur
 
 # Création de l'objet socket de type TCP (SOCK_STREAM)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connexion au serveur
 s.connect((host, port))
-
-print(f"Connecté avec succès au serveur {host} sur le port {port}")
-
 # note : la double parenthèse n'est pas une erreur : on envoie un tuple à la fonction connect()
 
+print(f'Connecté avec succès au serveur {host} sur le port {port}')
+
 # Envoi de data bidon
-s.sendall(b'SALUT MEC')
+input_data = input("Que veut tu envoyé au serveur : ")
+s.sendall(input_data.encode())
 
 # On reçoit 1024 bytes qui contiennent peut-être une réponse du serveur
 data = s.recv(1024)
@@ -24,7 +24,6 @@ data = s.recv(1024)
 # On libère le socket TCP
 s.close()
 
-# Affichage de la réponse reçue du serveur
-print(f"Meooooooo")
+print(repr(data))
 
 sys.exit(0)
