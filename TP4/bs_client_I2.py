e17f1fe7ae31ce2fe8a0c24b8786bcb5b802eed9
MARIAMON <smaintos@MacBook-Air-de-smaintos.local>
2023-11-20 10:00:41 +0100

    modif

--- TP4/bs_client_I2.py
+++ TP4/bs_client_I2.py
@@ -1,29 +1,37 @@
 import socket
 import sys
 
-# On définit la destination de la connexion
-host = '172.16.40.8'  # IP du serveur
-port = 13337               # Port choisir par le serveur
+while True:
+    # On définit la destination de la connexion
+    host = '172.16.40.8'  # IP du serveur
+    port = 13337  # Port choisir par le serveur
 
-# Création de l'objet socket de type TCP (SOCK_STREAM)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Création de l'objet socket de type TCP (SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connexion au serveur
-s.connect((host, port))
-# note : la double parenthèse n'est pas une erreur : on envoie un tuple à la fonction connect()
+    try:
+        # Connexion au serveur
+        s.connect((host, port))
+        # note : la double parenthèse n'est pas une erreur : on envoie un tuple à la fonction connect()
 
-print(f'Connecté avec succès au serveur {host} sur le port {port}')
+        print(f'Connecté avec succès au serveur {host} sur le port {port}')
 
-# Envoi de data bidon
-input_data = input("Que veut tu envoyé au serveur : ")
-s.sendall(input_data.encode())
+        # Envoi de data bidon
+        input_data = input("Que veux-tu envoyer au serveur ('exit' pour quitter) : ")
 
-# On reçoit 1024 bytes qui contiennent peut-être une réponse du serveur
-data = s.recv(1024)
+        if input_data.lower() == 'exit':
+            break  # Quitter la boucle si l'utilisateur entre 'exit'
 
-# On libère le socket TCP
-s.close()
+        s.sendall(input_data.encode())
 
-print(repr(data))
+        # On reçoit 1024 bytes qui contiennent peut-être une réponse du serveur
+        data = s.recv(1024)
 
-sys.exit(0)
+        print(repr(data))
+
+    except Exception as e:
+        print(f"Une erreur s'est produite : {e}")
+
+    finally:
+        # On libère le socket TCP
+        s.close()
