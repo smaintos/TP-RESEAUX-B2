b5d6e048b86fe6785e4c3a0ba6298c5ffee40931
MARIAMON <smaintos@MacBook-Air-de-smaintos.local>
2023-11-20 10:17:02 +0100

    client3

--- TP4/bs_client_I3.py
+++ TP4/bs_client_I3.py
@@ -3,39 +3,32 @@ import sys
 
 # On définit la destination de la connexion
 host = '172.16.40.8'  # IP du serveur
-port = 13337  # Port choisir par le serveur
+port = 13337               # Port choisir par le serveur
 
 # Création de l'objet socket de type TCP (SOCK_STREAM)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-try:
-    # Connexion au serveur
-    s.connect((host, port))
-    # note : la double parenthèse n'est pas une erreur : on envoie un tuple à la fonction connect()
+# Connexion au serveur
+s.connect((host, port))
+# note : la double parenthèse n'est pas une erreur : on envoie un tuple à la fonction connect()
 
-    print(f'Connecté avec succès au serveur {host} sur le port {port}')
+print(f'Connecté avec succès au serveur {host} sur le port {port}')
 
-    # Envoi de data bidon
-    input_data = input("Que veux-tu envoyer au serveur : ")
+# Envoi de data bidon
+input_data = input("Que veut tu envoyé au serveur : ")
+if type(input_data) is str & input_data == "waf" | input_data == "meo":
+    try:
+        s.sendall(input_data.encode())
+    except Exception as e:
+        print("Erreur lors de l'envoi des données")
+        print(e)
 
-    # Correction des conditions pour envoyer les données
-    if isinstance(input_data, str) and (input_data == "waf" or input_data == "meo"):
-        try:
-            s.sendall(input_data.encode())
-        except Exception as e:
-            print("Erreur lors de l'envoi des données")
-            print(e)
+# On reçoit 1024 bytes qui contiennent peut-être une réponse du serveur
+data = s.recv(1024)
 
-    # On reçoit 1024 bytes qui contiennent peut-être une réponse du serveur
-    data = s.recv(1024)
+# On libère le socket TCP
+s.close()
 
-    print(repr(data))
-
-except Exception as e:
-    print(f"Une erreur s'est produite : {e}")
-
-finally:
-    # On libère le socket TCP
-    s.close()
+print(repr(data))
 
 sys.exit(0)
