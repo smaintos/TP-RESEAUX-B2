d8340be063d7587c29f269d4befb0095e696ec79
MARIAMON <smaintos@MacBook-Air-de-smaintos.local>
2023-11-20 10:16:02 +0100

    aller

--- TP4/bs_client_I3.py
+++ TP4/bs_client_I3.py
@@ -3,32 +3,39 @@ import sys
 
 # On définit la destination de la connexion
 host = '172.16.40.8'  # IP du serveur
-port = 13337               # Port choisir par le serveur
+port = 13337  # Port choisir par le serveur
 
 # Création de l'objet socket de type TCP (SOCK_STREAM)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connexion au serveur
-s.connect((host, port))
-# note : la double parenthèse n'est pas une erreur : on envoie un tuple à la fonction connect()
+try:
+    # Connexion au serveur
+    s.connect((host, port))
+    # note : la double parenthèse n'est pas une erreur : on envoie un tuple à la fonction connect()
 
-print(f'Connecté avec succès au serveur {host} sur le port {port}')
+    print(f'Connecté avec succès au serveur {host} sur le port {port}')
 
-# Envoi de data bidon
-input_data = input("Que veut tu envoyé au serveur : ")
-if type(input_data) is str & input_data == "waf" | input_data == "meo":
-    try:
-        s.sendall(input_data.encode())
-    except Exception as e:
-        print("Erreur lors de l'envoi des données")
-        print(e)
+    # Envoi de data bidon
+    input_data = input("Que veux-tu envoyer au serveur : ")
 
-# On reçoit 1024 bytes qui contiennent peut-être une réponse du serveur
-data = s.recv(1024)
+    # Correction des conditions pour envoyer les données
+    if isinstance(input_data, str) and (input_data == "waf" or input_data == "meo"):
+        try:
+            s.sendall(input_data.encode())
+        except Exception as e:
+            print("Erreur lors de l'envoi des données")
+            print(e)
 
-# On libère le socket TCP
-s.close()
+    # On reçoit 1024 bytes qui contiennent peut-être une réponse du serveur
+    data = s.recv(1024)
 
-print(repr(data))
+    print(repr(data))
+
+except Exception as e:
+    print(f"Une erreur s'est produite : {e}")
+
+finally:
+    # On libère le socket TCP
+    s.close()
 
 sys.exit(0)
