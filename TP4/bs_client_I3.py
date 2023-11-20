602dbfd677a64dd4286916247e35adf2afa0876b
MARIAMON <smaintos@MacBook-Air-de-smaintos.local>
2023-11-20 11:06:47 +0100

    cbon

--- TP4/bs_client_I3.py
+++ TP4/bs_client_I3.py
@@ -1,9 +1,10 @@
 import socket
 import sys
+import re
 
 # On définit la destination de la connexion
 host = '172.16.40.8'  # IP du serveur
-port = 13337               # Port choisir par le serveur
+port = 13337               # Port choisi par le serveur
 
 # Création de l'objet socket de type TCP (SOCK_STREAM)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,13 +16,21 @@ s.connect((host, port))
 print(f'Connecté avec succès au serveur {host} sur le port {port}')
 
 # Envoi de data bidon
-input_data = input("Que veut tu envoyé au serveur : ")
-if type(input_data) is str & input_data == "waf" | input_data == "meo":
-    try:
-        s.sendall(input_data.encode())
-    except Exception as e:
-        print("Erreur lors de l'envoi des données")
-        print(e)
+input_data = input("Que veut tu envoyer au serveur : ")
+
+# Vérifier que l'entrée est une chaîne de caractères
+if not isinstance(input_data, str):
+    raise TypeError("Erreur : envoie du texte t'abuse ")
+
+# Vérifier que la chaîne contient soit "waf" soit "meo"
+if not re.match(r'^(waf|meo)$', input_data):
+    raise ValueError("Erreur : nannnnnnnn bro soit waf soit meo")
+
+try:
+    s.sendall(input_data.encode())
+except Exception as e:
+    print("Erreur lors de l'envoi des données")
+    print(e)
 
 # On reçoit 1024 bytes qui contiennent peut-être une réponse du serveur
 data = s.recv(1024)
