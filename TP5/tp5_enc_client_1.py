7ce60f906e0821d5f8faae8ae4062485bf1907b3
MARIAMON <smaintos@MacBook-Air-de-smaintos.local>
2023-11-30 14:30:54 +0100

    test encode

--- TP5/tp5_enc_client_1.py
+++ TP5/tp5_enc_client_1.py
@@ -1,39 +1,19 @@
 import socket
-import struct
 
-def send_expression(sock, expression):
-    # Encode l'expression en UTF-8
-    encoded_expression = expression.encode('utf-8')
-
-    # Calcule la taille de l'expression en octets
-    expression_len = len(encoded_expression)
-
-    # Crée l'en-tête avec la taille de l'expression
-    header = struct.pack("!I", expression_len)
-
-    # Envoie l'en-tête et l'expression
-    sock.send(header + encoded_expression)
-
-    # Envoie une séquence de fin (0)
-    sock.send(b'\x00')
-
-# Connexion au serveur
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('172.16.40.8', 13337))
+s.connect(('127.0.0.1', 9999))
 s.send('Hello'.encode())
 
 # On reçoit la string Hello
 data = s.recv(1024)
 
-# Récupération de l'expression arithmétique de l'utilisateur
-expression = input("Expression arithmétique à envoyer: ")
+# Récupération d'une string utilisateur
+msg = input("Calcul à envoyer: ")
 
-# Envoie l'expression au serveur
-send_expression(s, expression)
+# On envoie
+s.send(msg.encode())
 
 # Réception et affichage du résultat
 s_data = s.recv(1024)
 print(s_data.decode())
-
-# Fermeture de la connexion
 s.close()
--- TP5/tp5_enc_server_1.py
+++ TP5/tp5_enc_server_1.py
@@ -1,29 +1,14 @@
 import socket
-import struct
 
-def recv_expression(sock):
-    # Lit l'en-tête pour déterminer la taille du message
-    header = sock.recv(4)
-    expression_len = struct.unpack("!I", header)[0]
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('127.0.0.1', 9999))  
 
-    # Lit les octets suivants pour obtenir l'expression
-    expression = sock.recv(expression_len)
-
-    # Vérifie la séquence de fin
-    end_sequence = sock.recv(1)
-    if end_sequence != b'\x00':
-        raise ValueError("Séquence de fin incorrecte")
-
-    return expression.decode('utf-8')
-
-# Création de la socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('172.16.40.8', 13337))  
-server_socket.listen(1)
-conn, addr = server_socket.accept()
+s.listen(1)
+conn, addr = s.accept()
 
 while True:
+
     try:
         # On reçoit la string Hello du client
         data = conn.recv(1024)
@@ -32,22 +17,15 @@ while True:
 
         conn.send("Hello".encode())
 
-        # On reçoit l'expression arithmétique du client
-        expression = recv_expression(conn)
-
-        # Affiche la taille du message
-        print(f"Taille du message : {len(expression)} octets")
-
-        # Affiche l'expression
-        print(f"Expression reçue : {expression}")
+        # On reçoit le calcul du client
+        data = conn.recv(1024)
 
         # Evaluation et envoi du résultat
-        res = eval(expression)
+        res  = eval(data.decode())
         conn.send(str(res).encode())
          
     except socket.error:
         print("Error Occured.")
         break
 
-# Fermeture de la connexion
 conn.close()
