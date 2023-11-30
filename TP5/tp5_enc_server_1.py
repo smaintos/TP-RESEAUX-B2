553cccd6e87e38eaedf5d2c62b4366cf2fad542a
MARIAMON <smaintos@MacBook-Air-de-smaintos.local>
2023-11-30 15:41:46 +0100

    test encode

--- TP5/tp5_enc_server_1.py
+++ TP5/tp5_enc_server_1.py
@@ -1,31 +1,41 @@
 import socket
 
+def receive_with_header(sock):
+    # Reçoit un message avec un en-tête annonçant la taille du message
+    header = sock.recv(1024)
+    message_size = int(header.decode())
+    message = sock.recv(message_size).decode()
+    # Reçoit la séquence de fin
+    end_sequence = sock.recv(1024).decode()
+    if end_sequence != '<clafin>':
+        print("Séquence de fin incorrecte.")
+        return None
+    return message
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(('172.16.40.8', 13337))  
-
+s.bind(('172.16.40.8', 13337))
 s.listen(1)
 conn, addr = s.accept()
 
 while True:
-
     try:
-        # On reçoit la string Hello du client
-        data = conn.recv(1024)
-        if not data: break
-        print(f"Données reçues du client : {data}")
-
-        conn.send("Hello".encode())
-
-        # On reçoit le calcul du client
-        data = conn.recv(1024)
+        # On reçoit le calcul du client avec un en-tête de taille
+        data = receive_with_header(conn)
+        if not data:
+            break
+        print(f"Calcul reçu du client : {data}")
 
         # Evaluation et envoi du résultat
-        res  = eval(data.decode())
-        conn.send(str(res).encode())
-         
+        try:
+            res = eval(data)
+            conn.sendall(str(res).encode())
+        except Exception as e:
+            print(f"Erreur lors de l'évaluation du calcul : {e}")
+            conn.sendall("Erreur".encode())
+
     except socket.error:
-        print("Error Occured.")
+        print("Error Occurred.")
         break
 
 conn.close()
