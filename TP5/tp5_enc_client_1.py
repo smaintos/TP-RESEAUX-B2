63c20c06590c6509a6d3ff209f005efc0d2cb18e
MARIAMON <smaintos@MacBook-Air-de-smaintos.local>
2023-11-30 15:29:40 +0100

    test encode

--- TP5/tp5_enc_client_1.py
+++ TP5/tp5_enc_client_1.py
@@ -1,4 +1,6 @@
 import socket
+import select
+import sys
 
 def is_valid_expression(expr):
     # Vérifie si l'expression est une opération arithmétique simple
@@ -25,7 +27,6 @@ def send_with_header(sock, message):
     header = str(message_size).encode()
     sock.sendall(header + message.encode() + '<clafin>'.encode())
 
-
 def main():
     host = "172.16.40.8"  # Remplace avec l'adresse IP du serveur
     port = 13337
@@ -33,18 +34,25 @@ def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
 
+    inputs = [client_socket, sys.stdin]
+
     while True:
-        expression = input("Entrez une expression arithmétique simple (ex. 3 + 3): ")
+        readable, _, _ = select.select(inputs, [], [])
 
-        if is_valid_expression(expression):
-            # Envoyer l'expression au serveur avec un en-tête de taille
-            send_with_header(client_socket, expression)
+        for s in readable:
+            if s == client_socket:
+                # Le serveur a envoyé une réponse
+                response = s.recv(1024)
+                print(f"Réponse du serveur : {response.decode()}")
+            elif s == sys.stdin:
+                # L'utilisateur a entré une expression
+                expression = input("Entrez une expression arithmétique simple (ex. 3 + 3): ")
 
-            # Recevoir et imprimer la réponse du serveur
-            response = client_socket.recv(1024)
-            print(f"Réponse du serveur : {response.decode()}")
-        else:
-            print("Expression invalide. Veuillez entrer une expression arithmétique simple.")
+                if is_valid_expression(expression):
+                    # Envoyer l'expression au serveur avec un en-tête de taille
+                    send_with_header(client_socket, expression)
+                else:
+                    print("Expression invalide. Veuillez entrer une expression arithmétique simple.")
 
     client_socket.close()
 
