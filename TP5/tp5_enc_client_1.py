c5a5794b37aff83f071cb853c75d5a72fb00a50f
MARIAMON <smaintos@MacBook-Air-de-smaintos.local>
2023-11-30 15:12:17 +0100

    test encode

--- TP5/tp5_enc_client_1.py
+++ TP5/tp5_enc_client_1.py
@@ -1,19 +1,41 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('172.16.40.8', 13337))
-s.send('Hello'.encode())
+def is_valid_expression(expr):
+    # Vérifie si l'expression est une opération arithmétique simple
+    valid_operators = ['+', '-', '*']
+    parts = expr.split()
 
-# On reçoit la string Hello
-data = s.recv(1024)
+    if len(parts) != 3:
+        return False
 
-# Récupération d'une string utilisateur
-msg = input("Calcul à envoyer: ")
+    try:
+        x = int(parts[0])
+        y = int(parts[2])
+    except ValueError:
+        return False
 
-# On envoie
-s.send(msg.encode())
+    if parts[1] not in valid_operators:
+        return False
 
-# Réception et affichage du résultat
-s_data = s.recv(1024)
-print(s_data.decode())
-s.close()
+    return True
+
+def main():
+    host = "172.16.40.8"
+    port = 13337
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+
+    while True:
+        expression = input("Entrez une expression arithmétique simple (ex. 3 + 3): ")
+
+        if is_valid_expression(expression):
+            # Envoyer l'expression au serveur
+            client_socket.send(expression.encode())
+        else:
+            print("Expression invalide. Veuillez entrer une expression arithmétique simple.")
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
