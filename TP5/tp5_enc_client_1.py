afca9eff917e41ba2a829318dceac508645cdb79
MARIAMON <smaintos@MacBook-Air-de-smaintos.local>
2023-11-30 15:31:15 +0100

    test encode

--- TP5/tp5_enc_client_1.py
+++ TP5/tp5_enc_client_1.py
@@ -1,60 +1,19 @@
 import socket
-import select
-import sys
 
-def is_valid_expression(expr):
-    # Vérifie si l'expression est une opération arithmétique simple
-    valid_operators = ['+', '-', '*']
-    parts = expr.split()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 9999))
+s.send('Hello'.encode())
 
-    if len(parts) != 3:
-        return False
+# On reçoit la string Hello
+data = s.recv(1024)
 
-    try:
-        x = int(parts[0])
-        y = int(parts[2])
-    except ValueError:
-        return False
+# Récupération d'une string utilisateur
+msg = input("Calcul à envoyer: ")
 
-    if parts[1] not in valid_operators:
-        return False
+# On envoie
+s.send(msg.encode())
 
-    return True
-
-def send_with_header(sock, message):
-    # Envoie un message avec un en-tête annonçant la taille du message
-    message_size = len(message)
-    header = str(message_size).encode()
-    sock.sendall(header + message.encode() + '<clafin>'.encode())
-
-def main():
-    host = "172.16.40.8"  # Remplace avec l'adresse IP du serveur
-    port = 13337
-
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((host, port))
-
-    inputs = [client_socket, sys.stdin]
-
-    while True:
-        readable, _, _ = select.select(inputs, [], [])
-
-        for s in readable:
-            if s == client_socket:
-                # Le serveur a envoyé une réponse
-                response = s.recv(1024)
-                print(f"Réponse du serveur : {response.decode()}")
-            elif s == sys.stdin:
-                # L'utilisateur a entré une expression
-                expression = input("Entrez une expression arithmétique simple (ex. 3 + 3): ")
-
-                if is_valid_expression(expression):
-                    # Envoyer l'expression au serveur avec un en-tête de taille
-                    send_with_header(client_socket, expression)
-                else:
-                    print("Expression invalide. Veuillez entrer une expression arithmétique simple.")
-
-    client_socket.close()
-
-if __name__ == "__main__":
-    main()
+# Réception et affichage du résultat
+s_data = s.recv(1024)
+print(s_data.decode())
+s.close()
--- TP5/tp5_enc_server_1.py
+++ TP5/tp5_enc_server_1.py
@@ -1,36 +1,31 @@
 import socket
 
-def receive_with_header(sock):
-    # Reçoit un message avec un en-tête annonçant la taille du message
-    header = sock.recv(1024)
-    message_size = int(header.decode())
-    message = sock.recv(message_size).decode()
-    # Reçoit la séquence de fin
-    end_sequence = sock.recv(1024).decode()
-    if end_sequence != '<clafin>':
-        print("Séquence de fin incorrecte.")
-        return None
-    return message
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(('172.16.40.8', 13337))
+s.bind(('127.0.0.1', 9999))  
+
 s.listen(1)
 conn, addr = s.accept()
 
 while True:
+
     try:
-        # On reçoit le calcul du client avec un en-tête de taille
-        data = receive_with_header(conn)
-        if data is None:
-            break
-        print(f"Calcul reçu du client : {data}")
+        # On reçoit la string Hello du client
+        data = conn.recv(1024)
+        if not data: break
+        print(f"Données reçues du client : {data}")
+
+        conn.send("Hello".encode())
+
+        # On reçoit le calcul du client
+        data = conn.recv(1024)
 
         # Evaluation et envoi du résultat
-        res = eval(data)
-        conn.sendall(str(res).encode())
+        res  = eval(data.decode())
+        conn.send(str(res).encode())
+         
     except socket.error:
-        print("Error Occurred.")
+        print("Error Occured.")
         break
 
 conn.close()
