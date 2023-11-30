d111d5650a4c6314150e855e3f5b7655614c6076
MARIAMON <smaintos@MacBook-Air-de-smaintos.local>
2023-11-30 16:08:47 +0100

    test encode

--- TP5/tp5_enc_client_1.py
+++ TP5/tp5_enc_client_1.py
@@ -2,65 +2,74 @@ import socket
 from re import compile
 from math import ceil
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('172.16.40.8', 13337))
+def send_calculation(socket, calculation):
+    # Validation de la saisie utilisateur
+    is_calculation_valid_pattern = compile('^(\+)?([0-9]){1,10} (\+|-|\*) (\+)?([0-9]){1,10}$')
 
-# Récupération d'une string utilisateur
-calculation = input("Calcul à envoyer: ")
+    if not is_calculation_valid_pattern.match(calculation):
+        raise TypeError("Veuillez saisir un calcul valide (addition, soustraction ou multiplication) : choisir des nombres entiers compris entre 0 et 4294967294")
 
-is_calculation_valid_pattern = compile('^(\+)?([0-9]){1,10} (\+|-|\*) (\+)?([0-9]){1,10}$')
+    array = calculation.split(" ")
 
-if not is_calculation_valid_pattern.match(calculation):
-    raise TypeError("Veuillez saisir un calcul valide (addition, soustraction ou multiplication) : choisir des nombres entiers compris entre 0 et 4294967294")
+    if int(array[0]) >= 4294967295 or int(array[2]) >= 4294967295:
+        print("Les nombres saisis doivent être compris entre 0 et 4294967294")
+        exit(0)
 
-array = calculation.split(" ")
+    first_nb, operand, second_nb = int(array[0]), array[1], int(array[2])
 
-if int(array[0]) >= 4294967295  or int(array[2]) >= 4294967295:
-    print("Les nombres saisis doivent être compris entre 0 et 4294967294")
+    first_nb_len, second_nb_len = ceil(first_nb.bit_length() / 8.0), ceil(second_nb.bit_length() / 8.0)
+
+    if first_nb == 0:
+        first_nb_len = 1
+    if second_nb == 0:
+        second_nb_len = 1
+
+    operand = array[1]
+
+    if operand == "+":
+        operand = 0
+    elif operand == "-":
+        operand = 1
+    else:
+        operand = 2
+
+    header = first_nb_len.to_bytes(4, byteorder='big') + second_nb_len.to_bytes(4, byteorder='big')
+
+    byte_calculation = first_nb.to_bytes(first_nb_len, byteorder='big') + operand.to_bytes(1, byteorder='big') + second_nb.to_bytes(second_nb_len, byteorder='big')
+
+    sequence = header + byte_calculation
+
+    # On envoie
+    socket.send(sequence)
+
+def receive_result(socket):
+    # Réception et affichage du résultat
+    res_byte_len = int.from_bytes(socket.recv(4), byteorder='big')
+
+    is_negative = int.from_bytes(socket.recv(1), byteorder='big') == 1
+
+    res = int.from_bytes(socket.recv(res_byte_len), byteorder='big')
+
+    if is_negative:
+        res = -abs(res)
+
+    return res
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('172.16.40.8', 13337))
+
+    # Récupération d'une string utilisateur
+    calculation = input("Calcul à envoyer: ")
+
+    send_calculation(s, calculation)
+
+    result = receive_result(s)
+
+    print(f"Le résultat du calcul {calculation} est : {result}")
+
+    s.close()
     exit(0)
-    
-first_nb, operand, second_nb = int(array[0]), array[1], int(array[2])
 
-first_nb_len, second_nb_len = ceil(first_nb.bit_length()/8.0), ceil(second_nb.bit_length()/8.0)
-print(first_nb_len, second_nb_len)
-
-if first_nb == 0:
-    first_nb_len = 1
-if second_nb == 0:
-    second_nb_len = 1
-
-operand = array[1]
-
-if operand == "+":
-    operand = 0
-elif operand == "-":
-    operand = 1
-else:
-    operand = 2
-
-header = first_nb_len.to_bytes(4, byteorder='big') + second_nb_len.to_bytes(4, byteorder='big')
-
-byte_calculation = first_nb.to_bytes(first_nb_len, byteorder='big') + operand.to_bytes(1, byteorder='big') + second_nb.to_bytes(second_nb_len, byteorder='big')
-
-sequence = header + byte_calculation
-
-# On envoie
-s.send(sequence)
-
-# Réception et affichage du résultat
-res_byte_len = int.from_bytes(s.recv(4), byteorder='big')
-
-is_negative = None
-if int.from_bytes(s.recv(1), byteorder='big') == 1:
-    is_negative = True
-else:
-    is_negative = False
-    
-res = int.from_bytes(s.recv(res_byte_len), byteorder='big')
-if is_negative:
-    res = -abs(res)
-    
-print(f"Le résultat du calcul {calculation} est : {res}")
-
-s.close()
-exit(0)
+if __name__ == "__main__":
+    main()
--- TP5/tp5_enc_server_1.py
+++ TP5/tp5_enc_server_1.py
@@ -1,63 +1,77 @@
 import socket
 from math import ceil
 
-port = 13337
-ip_addr = '172.16.40.8'
+def receive_calculation(socket):
+    # On reçoit le calcul du client
+    first_nb_len = int.from_bytes(socket.recv(4), byteorder='big')
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((ip_addr, port))  
+    if not first_nb_len:
+        return None
 
-print(f"server started at port ")
+    second_nb_len = int.from_bytes(socket.recv(4), byteorder='big')
 
-s.listen(1)
+    first_nb = int.from_bytes(socket.recv(first_nb_len), byteorder='big')
+    operand = int.from_bytes(socket.recv(1), byteorder='big')
+    second_nb = int.from_bytes(socket.recv(second_nb_len), byteorder='big')
 
-while True:
-    
-    conn, addr = s.accept()
-    
-    print(f"client {addr[0]} is connected")
+    if operand == 0:
+        operand = "+"
+    elif operand == 1:
+        operand = "-"
+    else:
+        operand = "*"
 
-    try:
-        # On reçoit le calcul du client
-        first_nb_len = int.from_bytes(conn.recv(4), byteorder='big')
-        
-        if not first_nb_len:
-            continue
-        
-        second_nb_len = int.from_bytes(conn.recv(4), byteorder='big')
-        
-        first_nb = int.from_bytes(conn.recv(first_nb_len), byteorder='big')
-        operand = int.from_bytes(conn.recv(1), byteorder='big')
-        second_nb = int.from_bytes(conn.recv(second_nb_len), byteorder='big')
-        
-        if operand == 0:
-            operand = "+"
-        elif operand == 1:
-            operand = "-"
-        else:
-            operand = "*"
+    calculation = f"{first_nb} {operand} {second_nb}"
+    return calculation
 
-        calculation = f"{first_nb} {operand} {second_nb}"
-        
-        # Evaluation et envoi du résultat
-        res: int = eval(calculation)
-        
-        res_byte_len = ceil(res.bit_length()/8.0)
-        
-        header = res_byte_len.to_bytes(4, byteorder='big')
-        
-        if res < 0:
-            header += int.to_bytes(1,1, byteorder='big')
-            res = abs(res)
-        else:
-            header += int.to_bytes(0,1, byteorder='big')
-        
-        sequence = header + res.to_bytes(res_byte_len, byteorder='big')
-        
-        conn.send(sequence)
-         
-    except socket.error:
-        print("Error Occured.")
-        break
-    
-conn.close()
+def send_result(socket, result):
+    # Envoie du résultat au client
+    res_byte_len = ceil(result.bit_length() / 8.0)
+
+    header = res_byte_len.to_bytes(4, byteorder='big')
+
+    if result < 0:
+        header += int.to_bytes(1, 1, byteorder='big')
+        result = abs(result)
+    else:
+        header += int.to_bytes(0, 1, byteorder='big')
+
+    sequence = header + result.to_bytes(res_byte_len, byteorder='big')
+
+    socket.send(sequence)
+
+def main():
+    port = 13337
+    ip_addr = '172.16.40.8'
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((ip_addr, port))
+
+    print(f"Server started at port {port}")
+
+    s.listen(1)
+
+    while True:
+        conn, addr = s.accept()
+        print(f"Client {addr[0]} is connected")
+
+        try:
+            calculation = receive_calculation(conn)
+
+            if calculation is None:
+                continue
+
+            print(f"Calculation received from client: {calculation}")
+
+            # Evaluation et envoi du résultat
+            result = eval(calculation)
+            send_result(conn, result)
+
+        except socket.error:
+            print("Error Occurred.")
+            break
+
+    conn.close()
+
+if __name__ == "__main__":
+    main()
