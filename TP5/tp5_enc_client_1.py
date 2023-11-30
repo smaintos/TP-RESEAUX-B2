8a346cf6a2608f22f017613a0b6f6005b6bf6723
william <william.mariamon@ynov.com>
2023-11-30 23:16:09 +0100

    test encore

--- TP5/tp5_enc_client_1.py
+++ TP5/tp5_enc_client_1.py
@@ -1,4 +1,5 @@
 import socket
+from re import compile
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('10.2.2.3', 13337))
@@ -6,13 +7,12 @@ s.connect(('10.2.2.3', 13337))
 # Récupération d'une string utilisateur
 calc = input("Calcul à envoyer: ")
 
-if not (calc.count('+') == 1 and calc.count('-') <= 1 and calc.count('*') <= 1):
-    raise TypeError("Veuillez saisir un seul opérateur (+, - ou *) dans le calcul.")
+calc_pattern = compile('^(\+|-)?([0-9]){1,10} (\+|-|\*) (\+|-)?([0-9]){1,10}$')
+
+if not calc_pattern.match(calc):
+    raise TypeError("Veuillez saisir un calcul valide (addition, soustraction ou multiplication) : choisir des nombres entiers compris inférieurs à 4294967295")
 
 nums = calc.split(" ")
-
-if not all(num.lstrip('-').isdigit() for num in nums):
-    raise TypeError("Veuillez saisir des nombres entiers.")
 
 if int(nums[0]) >= 4294967295 or int(nums[2]) >= 4294967295:
     print("Les nombres saisis doivent être inférieurs à 4294967295")
@@ -22,9 +22,9 @@ n1, op, n2 = nums[0], nums[1], nums[2]
 
 n1_len, op_len, n2_len = len(n1), len(op), len(n2)
 
-header = n1_len.to_bytes(4, byteorder='big') + n2_len.to_bytes(4, byteorder='big') + op_len.to_bytes(4, byteorder='big')
+hdr = n1_len.to_bytes(4, byteorder='big') + n2_len.to_bytes(4, byteorder='big') + op_len.to_bytes(4, byteorder='big')
 
-seq = header + calc.replace(" ", "").encode()
+seq = hdr + calc.replace(" ", "").encode()
 print(seq)
 
 # On envoie
