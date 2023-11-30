1570d53e5e733d7e54242a19fd14cc167d51d0f5
MARIAMON <smaintos@MacBook-Air-de-smaintos.local>
2023-11-30 14:36:15 +0100

    test encode

--- TP5/tp5_enc_client_1.py
+++ TP5/tp5_enc_client_1.py
@@ -10,10 +10,23 @@ data = s.recv(1024)
 # Récupération d'une string utilisateur
 msg = input("Calcul à envoyer: ")
 
+# on encode le message explicitement en UTF-8 pour récup un tableau de bytes
+encoded_msg = msg.encode('utf-8')
+
+# on calcule sa taille, en nombre d'octets
+msg_len = len(encoded_msg)
+
+# on encode ce nombre d'octets sur une taille fixe de 4 octets
+header = msg_len.to_bytes(4, byteorder='big')
+
+# on peut concaténer ce header avec le message, avant d'envoyer sur le réseau
+payload = header + encoded_msg
+
+
 # On envoie
-s.send(msg.encode())
+s.send(payload)
 
 # Réception et affichage du résultat
-s_data = s.recv(1024)
+s_data = s.recv(payload)
 print(s_data.decode())
 s.close()
