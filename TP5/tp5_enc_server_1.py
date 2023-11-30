2ca602dff9daba4637c379e70d36a6dc02474071
MARIAMON <smaintos@MacBook-Air-de-smaintos.local>
2023-11-30 16:05:18 +0100

    test encode

--- TP5/tp5_enc_server_1.py
+++ TP5/tp5_enc_server_1.py
@@ -1,43 +1,63 @@
 import socket
+from math import ceil
 
-def receive_with_header(sock):
-    # Reçoit un message avec un en-tête annonçant la taille du message
-    header = sock.recv(1024)
-    message_size = int(header.decode())
-    message = sock.recv(message_size).decode()
-    return message
-
-def send_with_header(sock, message):
-    # Envoie un message avec un en-tête annonçant la taille du message
-    message_size = len(message)
-    header = str(message_size).encode()
-    sock.sendall(header + message.encode())
+port = 13337
+ip_addr = '172.16.40.8'
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(('172.16.40.8', 13337))
+s.bind((ip_addr, port))  
+
+print(f"server started at port ")
+
 s.listen(1)
-conn, addr = s.accept()
 
 while True:
+    
+    conn, addr = s.accept()
+    
+    print(f"client {addr[0]} is connected")
+
     try:
-        # On reçoit le message du client avec un en-tête de taille
-        data = receive_with_header(conn)
-        if not data:
-            break
+        # On reçoit le calcul du client
+        first_nb_len = int.from_bytes(conn.recv(4), byteorder='big')
+        
+        if not first_nb_len:
+            continue
+        
+        second_nb_len = int.from_bytes(conn.recv(4), byteorder='big')
+        
+        first_nb = int.from_bytes(conn.recv(first_nb_len), byteorder='big')
+        operand = int.from_bytes(conn.recv(1), byteorder='big')
+        second_nb = int.from_bytes(conn.recv(second_nb_len), byteorder='big')
+        
+        if operand == 0:
+            operand = "+"
+        elif operand == 1:
+            operand = "-"
+        else:
+            operand = "*"
 
-        print(f"Calcul reçu du client : {data}")
-
+        calculation = f"{first_nb} {operand} {second_nb}"
+        
         # Evaluation et envoi du résultat
-        try:
-            res = eval(data)
-            send_with_header(conn, str(res))
-        except Exception as e:
-            print(f"Erreur lors de l'évaluation du calcul : {e}")
-            send_with_header(conn, "Erreur")
-
+        res: int = eval(calculation)
+        
+        res_byte_len = ceil(res.bit_length()/8.0)
+        
+        header = res_byte_len.to_bytes(4, byteorder='big')
+        
+        if res < 0:
+            header += int.to_bytes(1,1, byteorder='big')
+            res = abs(res)
+        else:
+            header += int.to_bytes(0,1, byteorder='big')
+        
+        sequence = header + res.to_bytes(res_byte_len, byteorder='big')
+        
+        conn.send(sequence)
+         
     except socket.error:
-        print("Error Occurred.")
+        print("Error Occured.")
         break
-
+    
 conn.close()
