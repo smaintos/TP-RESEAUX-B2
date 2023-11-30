99ced7145548c521f42349386b2c62917be593dd
MARIAMON <smaintos@MacBook-Air-de-smaintos.local>
2023-11-30 14:50:16 +0100

    test encode

--- TP5/tp5_enc_client_1.py
+++ TP5/tp5_enc_client_1.py
@@ -1,7 +1,7 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('172.16.40.8', 13337))
+s.connect(('172.16.40.8', 1337))
 s.send('Hello'.encode())
 
 # On reçoit la string Hello
@@ -9,19 +9,6 @@ data = s.recv(1024)
 
 # Récupération d'une string utilisateur
 msg = input("Calcul à envoyer: ")
-
-# on encode le message explicitement en UTF-8 pour récup un tableau de bytes
-encoded_msg = msg.encode('utf-8')
-
-# on calcule sa taille, en nombre d'octets
-msg_len = len(encoded_msg)
-
-# on encode ce nombre d'octets sur une taille fixe de 4 octets
-header = msg_len.to_bytes(4, byteorder='big')
-
-# on peut concaténer ce header avec le message, avant d'envoyer sur le réseau
-payload = header + encoded_msg
-
 
 # On envoie
 s.send(msg.encode())
