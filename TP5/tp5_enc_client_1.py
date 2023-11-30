fbfa66f0eeee45977300df6e759f1bc13ca062de
MARIAMON <smaintos@MacBook-Air-de-smaintos.local>
2023-11-30 15:34:38 +0100

    test encode

--- TP5/tp5_enc_client_1.py
+++ TP5/tp5_enc_client_1.py
@@ -7,13 +7,25 @@ s.send('Hello'.encode())
 # On reçoit la string Hello
 data = s.recv(1024)
 
-# Récupération d'une string utilisateur
-msg = input("Calcul à envoyer: ")
+# Récupération d'une expression arithmétique de l'utilisateur
+expression = input("Entrez une expression arithmétique simple (ex. 3 + 3): ")
 
-# On envoie
-s.send(msg.encode())
+# Validation de l'expression
+try:
+    x, operator, y = map(str.strip, expression.split())
+    x = int(x)
+    y = int(y)
 
-# Réception et affichage du résultat
-s_data = s.recv(1024)
-print(s_data.decode())
+    if 0 <= x < 4294967295 and 0 <= y < 4294967295:
+        # Envoie de l'expression au serveur
+        s.send(expression.encode())
+
+        # Réception et affichage du résultat
+        s_data = s.recv(1024)
+        print(s_data.decode())
+    else:
+        print("Les nombres doivent être inférieurs à 4294967295.")
+except (ValueError, IndexError):
+    print("Expression invalide. Veuillez entrer une expression arithmétique simple.")
+
 s.close()
